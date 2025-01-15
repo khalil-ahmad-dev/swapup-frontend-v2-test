d34808b90bd813df19241d08facc952af14541b3
khalil-ahmad-dev <khalilahmaddev@gmail.com>
2025-01-15 22:43:11 +0500

    deploying the local build

---
+++ dist/assets/sign-CeCFtVFz.js
@@ -0,0 +1,1 @@
+import{v as n}from"./session-request-cOIzSP_d.js";import"./index-RevNN9Fo.js";async function o(e){const{account:s,params:a}=e;return n(s,a[1]),s.signMessage({message:{raw:a[0]}})}export{o as handleSignRequest};
