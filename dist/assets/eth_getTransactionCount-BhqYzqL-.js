58d8c16bbb20bedd7046c2bb7cd0498f80fe9fcf
khalil-ahmad-dev <khalilahmaddev>
2025-02-11 14:57:10 +0500

    deploying frontend to swapup2 resource group

---
+++ dist/assets/eth_getTransactionCount-BhqYzqL-.js
@@ -0,0 +1,1 @@
+import{a4 as n,ak as r}from"./index-CrUjKI9_.js";async function c(t,o){const e=await t({method:"eth_getTransactionCount",params:[o.address,o.blockNumber?n(o.blockNumber):o.blockTag||"pending"]});return r(e)}export{c as eth_getTransactionCount};
